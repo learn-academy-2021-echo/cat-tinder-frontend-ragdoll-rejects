77870ba3682c4a92886a2e93b80a3c291a9e0dbe
LEARN Academy <jeffreysjoshua@gmail.com>
2022-02-02 09:59:45 -0800

    frontend structure

---
+++ src/pages/CatShow.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class CatShow extends Component {
+  render() {
+    return (
+    <>
+      <h1>Check out this cat.</h1>
+    </>
+      );
+  }
+}
+export default CatShow
---
+++ src/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+ class NotFound extends Component {
+  render() {
+    return (
+    <>
+      <h1>Locked out? Climb wall. 404 </h1>
+    </>
+    )
+  }
+}
+export default NotFound
