5c276a2eaf03dc81fb7123202a7e4d1df3c324b1
LEARN Academy <jeffreysjoshua@gmail.com>
2022-02-04 10:26:10 -0800

    added more functionality

--- src/App.js
+++ src/App.js
@@ -44,7 +44,8 @@ class App extends Component{
             return <CatShow cat={cat} />
           }}
           />
-          <Route path="/catnew" component={CatNew} />
+          <Route path="/catnew" render={(props) => <CatNew createCat={this.createCat} />}
+           />
           <Route path="/catedit" component={CatEdit} />
           <Route component={NotFound}/>
         </Switch>
